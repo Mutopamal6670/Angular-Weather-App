e17b812f64a657d426a98fdb0325daa1ed81ab0c
Mutopamal6670 <malvinmutopa03@gmail.com>
2026-03-10 20:19:03 +0100

    Adding logic to the search component

--- src/app/Components/search/search.ts
+++ src/app/Components/search/search.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search.html',
   styleUrl: './search.css',
 })
 export class Search {
+  private readonly form_build = inject(FormBuilder);
 
+  readonly userForm = this.form_build.group({
+    city_name: this.form_build.control('', [Validators.required])
+  });
+
+  search(): void {
+    console.log(this.userForm.value);
+
+    //Add the logic of how you are going to handle the user input to display the waether forecast of the given city
+  }
 }
