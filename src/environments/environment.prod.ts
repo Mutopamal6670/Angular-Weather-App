9141714108b3a053b4682411fdecda6b50b95774
Mutopamal6670 <malvinmutopa03@gmail.com>
2026-03-13 13:07:46 +0100

    Added the environment folder and all the necessary services

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  weatherApiKey: 'b0543302af21973c44d8471bbda0a189' // can use the same key
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  weatherApiKey: '916bff41fbc3002dc2a480a58c5239fd' // replace with your actual OpenWeatherMap API key
+};
