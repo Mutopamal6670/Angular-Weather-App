fa6352d3891d596e3c77165557143fd622a3866c
Mutopamal6670 <malvinmutopa03@gmail.com>
2026-03-13 11:01:35 +0100

    Added event emission logic to the search component

--- src/app/Components/search/search.ts
+++ src/app/Components/search/search.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
@@ -8,15 +8,27 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
   styleUrl: './search.css',
 })
 export class Search {
+
   private readonly form_build = inject(FormBuilder);
 
+  @Output() readonly citySearch = new EventEmitter<string>();
+
   readonly userForm = this.form_build.group({
-    city_name: this.form_build.control('', [Validators.required])
+    city_name: this.form_build.control('', [Validators.required, Validators.minLength(2)])
   });
 
   search(): void {
     console.log(this.userForm.value);
 
     //Add the logic of how you are going to handle the user input to display the waether forecast of the given city
+    if(this.userForm.invalid) return;
+
+    const city = this.userForm.controls.city_name.value;
+
+    console.log(city);  //remove after project completion
+
+    if(city) {
+      this.citySearch.emit(city);
+    }
   }
 }
