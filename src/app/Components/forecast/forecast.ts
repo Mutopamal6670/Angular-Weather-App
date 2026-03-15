66cd98809d02821d3b15b4ba71aad2feb511c438
Mutopamal6670 <malvinmutopa03@gmail.com>
2026-03-16 00:26:27 +0100

    Added preliminary logic to the forecast component

--- src/app/Components/forecast/forecast.ts
+++ src/app/Components/forecast/forecast.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { WeatherForecast } from '../../Models/weather.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-forecast',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './forecast.html',
   styleUrl: './forecast.css',
 })
 export class Forecast {
+  @Input() forecast? : WeatherForecast | null = null;
 
 }
