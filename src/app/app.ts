0027e19c81817f5b2d7aa420f021b1918677a678
Mutopamal6670 <malvinmutopa03@gmail.com>
2026-04-05 22:07:41 +0200

    Modified the app template and added two new components: navigation and footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navigation } from "./Components/navigation/navigation";
+import { Footer } from "./Components/footer/footer";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navigation, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
