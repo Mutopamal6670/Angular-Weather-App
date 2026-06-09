d22aeae1ea4be48ee82544245a343dedc3b15f06
Mutopamal6670 <malvinmutopa03@gmail.com>
2026-06-09 15:38:13 +0100

    Modified the view a little bit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Home } from './Pages/home/home';
 
 export const routes: Routes = [
     { path: '', component: Home },
+    { path: 'home', component: Home},
     { path: '**', redirectTo: '' }
 ];
